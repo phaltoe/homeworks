587fc85a5d94ca309eb5776a6b201817a75c40ed
Pedro Henrique Altoé <phaltoe85@gmail.com>
2019-08-15 12:36:43 -0300

    Stack DS working

--- stack.rb
+++ stack.rb
@@ -1,17 +1,24 @@
 class Stack
+  attr_reader :stack
   def initialize
     # create ivar to store stack here!
+    @stack = Array.new
   end
 
   def push(el)
     # adds an element to the stack
+    @stack << el
   end
 
   def pop
     # removes one element from the stack
+    @stack.pop
+    @stack
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
+    @stack[-1]
   end
 end
+
